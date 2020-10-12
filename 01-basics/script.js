37c3ef4ef483c07675d9f1dbcd891f124a0555ad
Johan Nordström <johan@digitalvillage.se>
2020-10-12 14:12:31 +0200

    String properties and methods.

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,3 +1,4 @@
+/*
 let userName = "Petra";
 let studentName = "Alicia Andersson";
 let examPoints = 42;
@@ -9,3 +10,21 @@ alert(helloMsg);
 
 let examMsg = examPoints + " points is your result";
 console.log(examMsg);
+*/
+
+let firstName; // string? number? boolean? object? WTAF?
+console.log("firstName after declaration, but before assignment:");
+console.log(firstName);
+
+firstName = "Johan";
+console.log("firstName after first assignment:");
+console.log(firstName);
+
+firstName = null;
+console.log("firstName after second assignment:");
+console.log(firstName);
+
+console.log(42);
+
+const uselessConstant = 1;
+console.log(uselessConstant);
---
+++ 02-string-methods/script.js
@@ -0,0 +1,31 @@
+let name = "Bengt-Olof";
+
+console.log("Name is:", name);
+console.log("Length of name:", name.length);
+
+console.log("Name SHOUTED is:", name.toUpperCase()); // "ALICIA"
+console.log("Name whispered is:", name.toLowerCase()); // "alicia"
+
+console.log("Name is:", name); // "Alicia"
+
+console.log("First char of name is:", name[0]);
+console.log("Second char of name is:", name[1]);
+
+console.log("Char 'c' is at index:", name.indexOf("c"));
+console.log("Third char of name is:", name[3]);
+
+console.log("First occurance of char 'i' is at index:", name.indexOf('i'));
+console.log("Last occurance of char 'i' is at index:", name.lastIndexOf('i'));
+
+// let result = name.replace('i', 'j');
+// console.log(result); // Aljcia
+
+// get part of string
+// let result = name.slice(0, 3); // Ali
+// let result = name.slice(3, -1); // ci
+
+let pizza_slice = name.slice(3, 5);
+console.log("PIZZA?", pizza_slice);
+
+let tinyName = name.substr(3, 5);
+console.log("tinyName:", tinyName);
