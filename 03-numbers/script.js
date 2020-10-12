4e9da7b314b495860d1db06146a7302eb59c6d42
Johan Nordström <johan@digitalvillage.se>
2020-10-12 15:31:37 +0200

    Numbers, numbers and numbers.

---
+++ 03-numbers/script.js
@@ -0,0 +1,39 @@
+/*
+let radius = 20;
+const pi = 3.1415;
+
+let result = radius ** 2;    // 20^2
+
+console.log("radius:", radius); // 20
+console.log("result:", result); // 400
+
+let circumference = pi * radius ** 2;
+console.log("circumerence of a circle with radius " + radius + " is:", circumference);
+
+console.log(10 / 3); // 3.333333333 = 3
+console.log(10 % 3); // 1
+*/
+
+/*
+let points = 5;
+
+let result = points + 1; // 6
+
+let laps = 1;
+laps = laps + 1; // 2
+laps = laps + 1; // 3
+
+laps++; // 4
+laps++; // 5
+laps++; // 6
+
+laps--; // 5
+
+console.log(laps);
+*/
+
+let points = 10;
+let result = ++points;
+
+console.log("points:", points); // 11
+console.log("result", result); // 11
