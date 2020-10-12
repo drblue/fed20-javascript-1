8547e7c810f887dadcca47eedede6ed5f76936b9
Johan Nordström <johan@digitalvillage.se>
2020-10-12 11:59:32 +0200

    Concatenation of strings and numbers.

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,11 +1,11 @@
 let userName = "Petra";
 let studentName = "Alicia Andersson";
+let examPoints = 42;
 
-userName = "Edwin";
-
-let helloMsg = "Hello " + userName + ", your next student is " + studentName;
-
-userName = "Peter";
+let helloMsg = "Hello " + studentName + ", your exam result is: " + examPoints + " points!";
 
 // alert(helloMsg + " " + userName + ", your next student is " + studentName);
 alert(helloMsg);
+
+let examMsg = examPoints + " points is your result";
+console.log(examMsg);
