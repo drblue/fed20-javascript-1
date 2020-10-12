9076ed2a0e70685f199b611a3dcda3c6f71c8621
Johan Nordström <johan@digitalvillage.se>
2020-10-12 11:34:44 +0200

    Variables and constants.

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,2 +1,10 @@
-alert("Hello again, world!");
-alert("I can haz coffee? ☕️🤩");
+var userName = "Johan";
+userName = "Pelle";
+
+let helloMsg = "Hello, Variable World!";
+let studentName = "Pelle Persson";
+studentName = "Per Persson";
+
+const pi = 3.14;
+pi = 2.54;
+alert(pi);
