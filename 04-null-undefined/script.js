42fbb1696e5f3da662c96f57b5046cd0ade80143
Johan Nordström <johan@digitalvillage.se>
2020-10-13 10:19:17 +0200

    Null vs undefined in Math operations.

---
+++ 04-null-undefined/script.js
@@ -0,0 +1,11 @@
+// let gdpr; // undefined
+// let gdpr = null; // not false, but not true either
+
+let x;
+let y = null;
+let xResult = x + 5;
+let yResult = y + 5;
+console.log("x:", x);
+console.log("y:", y);
+console.log("xResult:", xResult);
+console.log("yResult:", yResult);
