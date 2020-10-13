3ef35ecfd75eb51162c389e60ae63c587534dc0a
Johan Nordström <johan@digitalvillage.se>
2020-10-13 10:40:56 +0200

    Type Conversion.

---
+++ 05-type-conversion/script.js
@@ -0,0 +1,34 @@
+/*
+let sum = 100;
+console.log("sum:", sum, typeof sum);
+
+let points = 25;
+console.log("points:", points, typeof points);
+
+let name = "Ironman";
+console.log("name:", name, typeof name);
+
+let strPoints = "25";
+console.log("strPoints:", strPoints, typeof strPoints);
+
+
+let sumPoints = sum + points;
+console.log("sumPoints:", sumPoints, typeof sumPoints);
+
+let sumStrPoints = sum + strPoints;
+console.log("sumStrPoints:", sumStrPoints, typeof sumStrPoints);
+
+let sumNumberStrPoints = sum + Number(strPoints);
+console.log("sumNumberStrPoints:", sumNumberStrPoints, typeof sumNumberStrPoints);
+
+let sumNumberName = sum + Number(name);
+console.log("sumNumberName:", sumNumberName, typeof sumNumberName);
+*/
+
+// let result = Boolean(100); // true
+// let result = Boolean(1); // true
+// let result = Boolean(0); // false
+// let result = Boolean(-100); // true
+// let result = Boolean("i am groot"); // true
+let result = Boolean(""); // false
+console.log("result:", result, typeof result);
