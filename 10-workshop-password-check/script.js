2cf8944252bf65c217b74390a2df7ef5e3ad738b
Johan Nordström <johan@digitalvillage.se>
2020-10-16 11:02:51 +0200

    Count number of special chars in a password.

--- 10-workshop-password-check/script.js
+++ 10-workshop-password-check/script.js
@@ -6,7 +6,7 @@
 
 let password;
 // password = "password"; // inte giltigt
-password = "p@ssword"; // giltigt
+password = "p@ssw%rd"; // giltigt
 // password = "pa$$word"; // giltigt
 // password = "secretpassword"; // inte giltigt
 // password = "secret-password"; // giltigt
@@ -16,25 +16,27 @@ const specialChars = [
 	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
 ];
 
-let passwordHasSpecialChar = false;
+let specialCharsCount = 0;
 // loop over specialChars
 for (let i = 0; i < specialChars.length; i++) {
 	// does password include the char we're at?
 	if (password.includes(specialChars[i])) {
 		// yep, it did. set status variable to true!
-		passwordHasSpecialChar = true;
-		break;
+		specialCharsCount++;
 	}
 }
 
 console.log(`🕵🏻‍♂️ Checking password '${password}'`);
+console.log(`🔍 Found ${specialCharsCount} special chars in the password!`);
 
-if (
-		(password.length >= 8 && passwordHasSpecialChar)
-		|| (password.length >= 12 && password.includes('-'))
-		|| (password.length >= 16)
-	) {
+if (password.length >= 8 && specialCharsCount >= 2) {
 	console.log("- ✅ Great! Such password, much secure, very hard to crack!");
+
+} else if (password.length >= 12 && specialCharsCount >= 1) {
+	console.log("- ✅ Great! That's a pretty good password!");
+
+} else if (password.length >= 16) {
+	console.log("- ✅ Great! That's a long password!");
 
 } else {
 	console.log("- 🚨 Insecure password, my grandma can crack it!");
