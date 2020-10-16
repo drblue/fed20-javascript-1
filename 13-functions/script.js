f6312d326039461df36f4547808b6c816fcc88ca
Johan Nordström <johan@digitalvillage.se>
2020-10-16 15:01:31 +0200

    Fun with Functions 101.

--- 13-functions/script.js
+++ 13-functions/script.js
@@ -11,21 +11,48 @@ function sayHi() {
 // sayHi();
 
 // Function expression (an anonymous function is saved to a (scoped) constant variable)
-const sayBye = function() {
-	console.log("Baiiiii");
+const sayBye = function(name) {
+	console.log(`Bye ${name}`);
 }
-// sayBye();
+// sayBye("Johan");
+// sayBye("Fredrik");
 
-/*
-const PI = 3.1415;
+const greet = function(time, name = "Anonymous") {
+	alert(`Good ${time}, ${name}!`);
+}
+// greet("morning", "Johan");
+// greet("evening", "class");
+// greet("night", "all");
+// greet("morning");
+// let t = "morning";
+// let n = "Johan";
+// greet(t, n);  // greet("morning", "Johan");
+
+const greetingGenerator = function(time = "morning", name = "Anonymous") {
+	return `Good ${time}, ${name}`;
+}
+// let msg = greetingGenerator("evening", "class"); // msg = "Good evening, class";
+// console.log("msg", msg);
+
 let r;
 let a;
 
+const calcArea = function(radius) {
+	return radius ** 2 * Math.PI;
+}
+
+/*
 r = 10;
-a = r ** 2 * PI;
+a = calcArea(r);
 console.log(`The area of a circle with radius ${r} is ${a}.`);
 
 r = 20;
-a = r ** 2 * PI;
+a = calcArea(r);
 console.log(`The area of a circle with radius ${r} is ${a}.`);
 */
+
+let circles = [10, 15, 20, 30, 42];
+for (let i = 0; i < circles.length; i++) {
+	const a = calcArea(circles[i]);
+	console.log(`The area of a circle with radius ${circles[i]} is ${a}.`);
+}
