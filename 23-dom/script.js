26f5dd17b2186fd8882e759d64867a876df062da
Johan Nordström <johan@digitalvillage.se>
2020-10-22 15:04:35 +0200

    innerText vs innerHTML vs textContent (correct)

--- 23-dom/script.js
+++ 23-dom/script.js
@@ -10,6 +10,15 @@
 // });
 
 const errorDiv = document.querySelector('div.error');
+
+// innerHTML retrieves the content inside the element,
+// *INCLUDING* HTML-child-elements and whitespaces
 console.log("errorDiv innerHTML:", errorDiv.innerHTML);
+
+// innerText retrieves ONLY the *VISIBLE* text inside the element and
+// ALL text from child elements, and removes any whitespaces
 console.log("errorDiv innerText:", errorDiv.innerText);
+
+// textContent retrives ALL text from inside the element and ALL text
+// from child elements, including whitespaces
 console.log("errorDiv textContent:", errorDiv.textContent);
