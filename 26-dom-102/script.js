93ae6237d2e74e249b2651e14e51a133e0d799f6
Johan Nordström <johan@digitalvillage.se>
2020-10-26 13:28:48 +0100

    DOM 102.

--- 26-dom-102/script.js
+++ 26-dom-102/script.js
@@ -7,6 +7,19 @@ const listitemsEl = document.querySelector('ul');
 const listitems = document.querySelectorAll('li');
 const addButton = document.querySelector('#add');
 
+// add event listener to parent ul element
+listitemsEl.addEventListener('click', e => {
+	console.log("This is an UL click event", e);
+
+	// stop from bubbling
+	e.stopPropagation();
+
+	if (e.target.tagName === "LI") {
+		console.log("You've clicked on a list item!");
+		e.target.remove();
+	}
+});
+
 addButton.addEventListener('click', () => {
 	const listitem = document.createElement('li');
 	listitem.innerText = 'I am a new list item 👶🏻';
@@ -15,10 +28,18 @@ addButton.addEventListener('click', () => {
 	listitemsEl.append(listitem);
 });
 
+/*
 listitems.forEach(listitem => {
 	listitem.addEventListener('click', vadsomhelst => {
 		console.log("Will remove list item", vadsomhelst.target);
-		vadsomhelst.target.remove();
+		// vadsomhelst.target.remove();
 	});
 });
+*/
 
+// add click listener to link
+document.querySelector('a').addEventListener('click', e => {
+	e.preventDefault();
+
+	alert("You clicked a link!");
+})
