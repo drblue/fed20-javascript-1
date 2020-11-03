c80638286f64628714be09ee82db1b9fd749cea8
Johan Nordström <johan@digitalvillage.se>
2020-11-03 13:41:21 +0100

    Sorting of arrays.

--- 30-array-methods/script.js
+++ 30-array-methods/script.js
@@ -8,3 +8,21 @@
  * reduce()
  */
 
+/** numbers */
+const numbers = [ 47, 5, 21, 50, 24, 18, 47, 13, 28, 8, 18, 15, 11, 43, 5, 33 ];
+console.log("Numbers in chaos:", numbers);
+
+// sort(?) numbers
+numbers.sort(); // FAIL!
+
+console.log("Numbers in proper(?) order:", numbers);
+
+
+/** names */
+const names = ["Johan", "Fredrik", "Peter", "Alicia", "Maja", "Elliot"];
+console.log("Names in chaos:", names);
+
+// make pretty
+names.sort();
+
+console.log("Names in proper(!) order:", names);
