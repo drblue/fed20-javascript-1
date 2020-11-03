f3912c7ebf436734400cfb1aa1477456f4a163f2
Johan Nordström <johan@digitalvillage.se>
2020-11-03 14:14:37 +0100

    Sorting of objects! 🤯.

--- 30-array-methods/script.js
+++ 30-array-methods/script.js
@@ -10,7 +10,7 @@
 
 /** numbers */
 const numbers = [ 47, 5, 21, 50, 24, 18, 47, 13, 28, 8, 18, 15, 11, 43, 5, 33 ];
-console.log("Numbers in chaos:", numbers);
+// console.log("Numbers in chaos:", numbers);
 
 // sort numbers THE CORRECT WAY
 numbers.sort((a, b) => a - b);
@@ -34,14 +34,64 @@ numbers.sort((a, b) => {
 });
 */
 
-console.log("Numbers in proper order:", numbers);
+// console.log("Numbers in proper order:", numbers);
 
 
 /** names */
 const names = ["Johan", "Fredrik", "Peter", "Alicia", "Maja", "Elliot"];
-console.log("Names in chaos:", names);
+// console.log("Names in chaos:", names);
 
 // make pretty
 names.sort();
 
-console.log("Names in proper(!) order:", names);
+// console.log("Names in proper(!) order:", names);
+
+/** sorting of objects also works! */
+const students = [
+	{
+		name: "Johan",
+		points: 1337,
+	},
+	{
+		name: "Peter",
+		points: 3,
+	},
+	{
+		name: "Alicia",
+		points: 42,
+	},
+	{
+		name: "Elliot",
+		points: 88,
+	},
+	{
+		name: "Maja",
+		points: 35,
+	},
+];
+/*
+students.sort((a, b) => {
+	if (a.points < b.points) {
+		return -1;
+	}
+
+	if (a.points > b.points) {
+		return 1;
+	}
+
+	return 0;
+});
+*/
+
+// sort students with lowest score first
+/*
+students.sort((a, b) => {
+	return (a.points - b.points);
+});
+*/
+
+// sort students with HIGHEST score first
+students.sort((a, b) => {
+	return (b.points - a.points);
+});
+console.log("sorted BEST students first:", students)
