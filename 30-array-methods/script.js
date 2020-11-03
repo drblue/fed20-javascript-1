54d288895ee7ea4bef1fe4987835d300b55cd978
Johan Nordström <johan@digitalvillage.se>
2020-11-03 13:57:59 +0100

    EVEN SHORTER sorting of numbers!.

--- 30-array-methods/script.js
+++ 30-array-methods/script.js
@@ -13,10 +13,11 @@ const numbers = [ 47, 5, 21, 50, 24, 18, 47, 13, 28, 8, 18, 15, 11, 43, 5, 33 ];
 console.log("Numbers in chaos:", numbers);
 
 // sort numbers THE CORRECT WAY
+numbers.sort((a, b) => a - b);
+/*
 numbers.sort((a, b) => {
 	return (a - b);  // if a < b, return negative value, if a > b, return positive, otherwise 0
 
-	/*
 	// if a is less than b (i.e. it should be sorted BEFORE b), return -1
 	if (a < b) {
 		return -1;
@@ -30,8 +31,8 @@ numbers.sort((a, b) => {
 	// if we've arrived here, then a is neither less than b, nor greater than b
 	// i.e., a is equal to b
 	return 0;
-	*/
 });
+*/
 
 console.log("Numbers in proper order:", numbers);
 
