6b0b7a19bd519c63423c70d774fe14bab6090fa8
Johan Nordström <johan@digitalvillage.se>
2020-11-04 10:26:27 +0100

    Render incompleted todos in a separate list.

--- 31-todos/todos.js
+++ 31-todos/todos.js
@@ -26,11 +26,14 @@ const todos = [
 ];
 
 const renderTodos = () => {
-	// empty HTML list of todos
+	// create variable to store temporary HTML, before passing it to the DOM
 	let html = "";
 
-	// loop over each todo and create a list-item for each todo, and add them to the HTML list
-	todos.forEach((todo, index) => {
+	// loop over todos and get every todo which is NOT YET completed
+	const incompleteTodos = todos.filter(todo => !todo.completed);
+
+	// loop over each INCOMPLETE todo and create a list-item for each todo
+	incompleteTodos.forEach((todo, index) => {
 		html += `
 			<li class="todo list-group-item ${todo.completed ? 'completed' : 'incomplete'}" data-index="${index}">
 				<span class="todo-title">${todo.title}</span>
@@ -39,38 +42,60 @@ const renderTodos = () => {
 		`;
 	});
 
-	todosEl.innerHTML = html;
+	// output resulting HTML of INCOMPLETE todos to DOM
+	document.querySelector('#todos').innerHTML = html;
+
+	// empty variable of temporary HTML
+	html = "";
+
+	// loop over todos and get every todo which IS completed
+	const completedTodos = todos.filter(todo => todo.completed);
+
+	// loop over each COMPLETED todo and create a list-item for each todo
+	completedTodos.forEach((todo, index) => {
+		html += `
+			<li class="todo list-group-item ${todo.completed ? 'completed' : 'incomplete'}" data-index="${index}">
+				<span class="todo-title">${todo.title}</span>
+				<button class="btn btn-danger btn-sm">X</button>
+			</li>
+		`;
+	});
+
+	// output resulting HTML of COMPLETED todos to DOM
+	document.querySelector('#completed-todos').innerHTML = html;
 }
 renderTodos();
 
-// add click listener to the actual list, and check if the clicked target is
-// a list-item, and only then remove it
-todosEl.addEventListener('click', e => {
-	if (e.target.tagName === "SPAN") {
-		// user clicked on a todo title, so toggle its `completed` status
+// add click listener to both todo-lists, and check if the clicked target is
+// a list-item, and take appropriate action
+document.querySelectorAll('.todos').forEach(todolist => {
+	todolist.addEventListener('click', e => {
+		if (e.target.tagName === "SPAN") {
+			// user clicked on a todo title, so toggle its `completed` status
 
-		// get index from parent `li`-element
-		const todoIndex = e.target.parentElement.dataset.index; // `<li data-index>`
+			// get index from parent `li`-element
+			const todoIndex = e.target.parentElement.dataset.index; // `<li data-index>`
 
-		// get todo from todos-array
-		const todo = todos[todoIndex];
+			// get todo from todos-array
+			const todo = todos[todoIndex];
 
-		// invert its completed-status
-		todo.completed = !todo.completed;
+			// invert its completed-status
+			todo.completed = !todo.completed;
 
-		// render new todo-list
-		renderTodos();
+			// render new todo-list
+			renderTodos();
 
-	} else if (e.target.tagName === "BUTTON") {
-		// user clicked on the big red X-button, so we need to ask our parent
-		// for the index of the todo
-		const todoIndex = e.target.parentElement.dataset.index;
-		todos.splice(todoIndex, 1);
+		} else if (e.target.tagName === "BUTTON") {
+			// user clicked on the big red X-button, so we need to ask our parent
+			// for the index of the todo
+			const todoIndex = e.target.parentElement.dataset.index;
+			todos.splice(todoIndex, 1);
 
-		// render new todo-list
-		renderTodos();
-	}
-});
+			// render new todo-list
+			renderTodos();
+		}
+	});
+})
 
 // handle user submitting the "New Todo" form
 newTodoFormEl.addEventListener('submit', e => {
