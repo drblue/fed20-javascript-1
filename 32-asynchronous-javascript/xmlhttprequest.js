a11424a89e1259a7b4f55af2723cf047fb877407
Johan Nordström <johan@digitalvillage.se>
2020-11-09 14:40:03 +0100

    Let callback decide what to do with the data.

--- 32-asynchronous-javascript/xmlhttprequest.js
+++ 32-asynchronous-javascript/xmlhttprequest.js
@@ -5,46 +5,38 @@
  *
  */
 
-const request = new XMLHttpRequest();
+const getPosts = (callback) => {
+	const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-	if (request.readyState === 4) {
-		if (request.status === 200) {
-			const response = JSON.parse(request.responseText);
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				const response = JSON.parse(request.responseText);
 
-			response.forEach(item => {
-				document.querySelector('#posts').innerHTML += `<li>${item.title}</li>`
-			});
+				// send response to callback
+				callback(response);
 
-		} else {
-			console.log("Request was unsuccessful 🥺");
+			} else {
+				console.log("Request was unsuccessful 🥺");
+			}
 		}
-	}
-});
+	});
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-request.send();
+	request.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+	request.send();
+}
 
-/**
- * GET USERS
- */
+const renderPostsToHTML = (posts) => {
+	posts.forEach(item => {
+		document.querySelector('#posts').innerHTML += `<li>${item.title}</li>`
+	});
+}
 
-const requestUsers = new XMLHttpRequest();
+const consoleLogPostTitles = (posts) => {
+	posts.forEach(item => {
+		console.log(item.title);
+	});
+}
 
-requestUsers.addEventListener('readystatechange', () => {
-	if (requestUsers.readyState === 4) {
-		if (requestUsers.status === 200) {
-			const response = JSON.parse(requestUsers.responseText);
-
-			response.forEach(item => {
-				document.querySelector('#users').innerHTML += `<li>${item.name}</li>`
-			});
-
-		} else {
-			console.log("Request for users was unsuccessful 🥺");
-		}
-	}
-});
-
-requestUsers.open('GET', 'https://jsonplaceholder.typicode.com/users');
-requestUsers.send();
+getPosts(renderPostsToHTML);
+getPosts(consoleLogPostTitles);
