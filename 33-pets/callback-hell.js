aba2a60b4d8bc0b972d141e6ce6de6e5508fce35
Johan Nordström <johan@digitalvillage.se>
2020-11-10 12:17:15 +0100

    Example of "Callback Hell", a.k.a. "JavaScript Triangle of Doom".

--- 33-pets/callback-hell.js
+++ 33-pets/callback-hell.js
@@ -3,3 +3,36 @@
  *
  */
 
+const getJSON = (url, callback) => {
+	const request = new XMLHttpRequest();
+
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				const response = JSON.parse(request.responseText);
+
+				// send response to callback
+				callback(undefined, response);
+
+			} else {
+				callback(request.status);
+			}
+		}
+	});
+
+	request.open('GET', url);
+	request.send();
+}
+
+getJSON('pets/birds.json', (err, birds) => {
+	getJSON('pets/cats.json', (err, cats) => {
+		getJSON('pets/dogs.json', (err, dogs) => {
+			getJSON('pets/hamsters.json', (err, hamsters) => {
+				getJSON('pets/bunnies.json', (err, bunnies) => {
+					// THEY'RE SO FLUFFY!
+
+				});
+			});
+		})
+	})
+});
