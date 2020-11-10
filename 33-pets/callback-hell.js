8a85b33c5d8da98c79fb963dca3de768ca3f0083
Johan Nordström <johan@digitalvillage.se>
2020-11-10 12:11:13 +0100

    Get first file, THEN get all files from the first file's content.

---
+++ 33-pets/callback-hell.js
@@ -0,0 +1,5 @@
+/**
+ * CALLBACK HELL
+ *
+ */
+
--- 33-pets/script.js
+++ 33-pets/script.js
@@ -24,8 +24,6 @@ const getJSON = (url, callback) => {
 	request.send();
 }
 
-let ourPets;
-
 console.log("Getting all pets...");
 getJSON('pets/pets.json', (err, res) => {
 	console.log("Got the pets!");
@@ -34,13 +32,21 @@ getJSON('pets/pets.json', (err, res) => {
 		document.querySelector('#errors').innerHTML += `<div class="alert alert-warning">Y U HAVE NO PETS? ERROR CODE ${err}!</div>`;
 
 	} else {
-		// start a request to each item in result
-		ourPets = res;
+		// iterate over result
+		res.forEach(species => {
+			// start a request to species.url
+			console.log(`Fetching ${species.type}...`);
+
+			getJSON(species.url, (err, animals) => {
+				console.log(`Got ${species.type}...`);
+
+				const output =
+					animals
+						.map(animal => `<li>${animal.name} is ${animal.age} years old</li>`)
+						.join('');
+
+				document.querySelector('#output').innerHTML += `<h2>${species.type}</h2><ul>${output}</ul>`;
+			});
+		});
 	}
 });
-
-ourPets.forEach(pet => {
-	getJSON(pet.url, (err, pets) => {
-		console.log("Got moar pets!", pets);
-	})
-})
