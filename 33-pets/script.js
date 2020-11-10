6858ee9a07857364a48eacd3543df20ba8de4a04
Johan Nordström <johan@digitalvillage.se>
2020-11-10 09:35:43 +0100

    Pets boilerplate.

---
+++ 33-pets/script.js
@@ -0,0 +1,25 @@
+/**
+ * PETS
+ *
+ */
+
+const getJSON = (url, callback) => {
+	const request = new XMLHttpRequest();
+
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState === 4) {
+			if (request.status === 200) {
+				const response = JSON.parse(request.responseText);
+
+				// send response to callback
+				callback(undefined, response);
+
+			} else {
+				callback(request.status);
+			}
+		}
+	});
+
+	request.open('GET', url);
+	request.send();
+}
