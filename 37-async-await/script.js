ec5c0d2891cd61766256b44b2f4c87a7e86184f4
Johan Nordström <johan@digitalvillage.se>
2020-11-12 11:57:57 +0100

    async/await.

--- 37-async-await/script.js
+++ 37-async-await/script.js
@@ -32,6 +32,7 @@ fetch('pets/pets.json')
 	});
 */
 
+/*
 // WILL WORK, BUT IS UGLY
 fetch('pets/pets.json')
 	.then(res => res.json())
@@ -58,3 +59,73 @@ fetch('pets/pets.json')
 	.catch(err => {
 		console.error("Something went wrong with the fetching of the pets", err);
 	});
+*/
+
+/*
+const hello = () => {
+	return "Hello";
+}
+
+const promisedHello = () => {
+	return new Promise((resolve, reject) => {
+		resolve("Hello");
+	});
+}
+
+async function oldHello() {
+	return "Hello";
+}
+
+const declarationHello = async function() {
+	return "Hello";
+}
+
+const aHello = async () => {
+	return "Hello from asyncHello function!";
+}
+aHello()
+	.then(data => console.log("Success!", data))
+	.catch(err => console.error("Promise was rejected!", err));
+*/
+
+const getDogs = async () => {
+	const response = await fetch('pets/dogs.json');
+	const data = await response.json();
+
+	return data;
+}
+
+const run = async () => {
+	const dogs = await getDogs();
+	console.log("Got dogs:", dogs);
+}
+run();
+
+/*
+// WILL WORK, BUT IS UGLY
+fetch('pets/pets.json')
+	.then(res => res.json())
+	.then(pets => {
+		fetch('pets/dogs.json')
+			.then(res => res.json())
+			.then(dogs => {
+				fetch('pets/cats.json')
+					.then(res => res.json())
+					.then(cats => {
+						fetch('pets/birds.json')
+							.then(res => res.json())
+							.then(birds => {
+								// FINALLY, all done
+								console.log("FINALLY, all done!");
+								console.log("Pets:", pets);
+								console.log("Dogs:", dogs);
+								console.log("Cats:", cats);
+								console.log("Birds:", birds);
+							})
+					})
+			})
+	})
+	.catch(err => {
+		console.error("Something went wrong with the fetching of the pets", err);
+	});
+*/
